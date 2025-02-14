984a6f7d6455315dc1639529d115a0c5876f3239
miguelzapaton <miguelzapaton@users.noreply.github.com>
2025-02-14 20:51:55 +0100

    Fix bootstrap-vue examples

--- examples/es/example-4/src/main.js
+++ examples/es/example-4/src/main.js
@@ -5,4 +5,9 @@ import App from './App.vue'
 import {vfjsPlugin} from 'vue3-form-json-schema';
 import {createBootstrap} from 'bootstrap-vue-next';
 
-createApp(App).use(createBootstrap()).use(vfjsPlugin).mount('#app');
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+
+import { BFormInput } from 'bootstrap-vue-next/components';
+
+createApp(App).use(createBootstrap()).component('BFormInput', BFormInput).use(vfjsPlugin).mount('#app');
